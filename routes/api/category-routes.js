db51f186c96f96db06b5eea43fd2c44afc7cc8de
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-24 20:00:49 -0300

    create routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,44 +9,61 @@ router.get('/', async (req, res) => {
       include: Product,
     });
 
-    res.json(categories)
+    res.json(categories);
   } catch (err) {
     console.error(err)
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 
-  console.log(req.params, req.body, res.body)
+  // console.log(req.params, req.body, res.body)
 });
 
 router.get('/:id', async (req, res) => {
   try {
+    const category = await Category.findByPk(req.params.id, {
+      include: Product,
+    })
+    !category ? res.status(404).json({ message: 'Oops! Category not found.' }) :
+      res.json(category);
 
   } catch (err) {
-
+    res.status(500).json(err);
   }
 });
 
 router.post('/', async (req, res) => {
   try {
-
+    const category = await Category.create(req.body);
+    res.json(category);
   } catch (err) {
-
+    res.status(500).json(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
   try {
-
+    const category = await Category.findByPk(req.params.id);
+  // did an if statement instead of ternary op because ternary op's dont work with await
+    if (!category) {
+      return res.status(404).json({ message: 'Oops! Category not found.' });
+    }
+    await category.update(req.body);
+    res.json(category);
   } catch (err) {
-
+    res.status(500).json(err);
   }
 });
 
 router.delete('/:id', async (req, res) => {
   try {
-
+    const category = await Category.findByPk(req.params.id)
+    if (!category) {
+      return res.status(404).json({ message: 'Oops! Category not found.' });
+    }
+    await category.destroy(req.body);
+    res.json({ message: 'Category deleted.' });
   } catch (err) {
-
+    res.status(500).json(err);
   }
 });
 
