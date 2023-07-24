683e99e364b4e407df73dcec20a9d4fa07df3219
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-24 12:23:01 -0300

    import sequalize and models, sync sequalize

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection')
+// import models
+const Category = require('./models/Category')
+const Product = require('./models/Product')
+const ProductTag = require('./models/ProductTag')
+const Tag = require('./models/Tag')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// sync sequelize models to the database force true to recreate tables on every sync, then turn on the server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`listening at ${PORT}`));
 });
