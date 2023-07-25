cf93c0569c0f2a98f10a5d2d609259f72a7cc555
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-25 18:26:52 -0300

    remove useless imports, add 'BelongsTo'

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection')
-// import models
-const Category = require('./models/Category')
-const Product = require('./models/Product')
-const ProductTag = require('./models/ProductTag')
-const Tag = require('./models/Tag')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
